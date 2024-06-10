04658dcffb86896591c54d04f911a7687037611a
Kikelomo855 <kikelomohelen22@gmail>
2024-06-10 16:55:02 +0100

    first bash and terraform class

---
+++ Assigment.sh
@@ -0,0 +1,34 @@
+echo "Enter your name:"
+read name
+echo "Hello, $name!" >> output.var
+
+
+echo "Enter your age:"
+read age
+echo "Age- $age!" >> output.var
+
+
+echo "Enter your address:"
+read address
+echo "Address- $address!" >> ouput.var
+
+
+echo "Enter your phone number:"
+read phonenumber
+echo "Phone number, $phonenumber!" >> output.var
+
+
+echo "Enter your sex:"
+read sex
+echo "Sex- $sex!" >> output.var
+
+
+echo "Enter your marital status:"
+read maritalstatus
+echo "Marital status- $maritalstatus!" >> output.var
+
+
+echo "Enter your occupation:"
+read occupation
+echo "Occupation- $occupation!" >> output.var 
+
---
+++ class.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# MyName="Helen, Kike, Victor, Moyin, Daniel"
+# echo $MyName
+# name="World"
+# echo "Hello $name"
+# echo "The date is $(date)" 
+
+# Declare variables
+# name="Kikelomo"
+# age=64
+# city="Lagos"
+
+# # Print out the values stored
+# echo "Name: $name"
+# echo "Age: $age"
+
